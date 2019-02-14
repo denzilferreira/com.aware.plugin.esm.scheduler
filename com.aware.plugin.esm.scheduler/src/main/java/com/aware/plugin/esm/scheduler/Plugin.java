81f8e04c0fad40ce8478880c78482fc54b2fc5a5
Denzil Ferreira <ferreiradenzil@gmail.com>
2019-02-14 12:03:21 +0200

    - Bump to 4.0.806 - Fix keyboard input for web ESM

--- com.aware.plugin.esm.scheduler/src/main/java/com/aware/plugin/esm/scheduler/Plugin.java
+++ com.aware.plugin.esm.scheduler/src/main/java/com/aware/plugin/esm/scheduler/Plugin.java
@@ -11,13 +11,10 @@ import android.os.Handler;
 import android.provider.CalendarContract;
 import android.text.Html;
 import android.util.Log;
-
 import com.aware.Aware;
-import com.aware.Aware_Preferences;
 import com.aware.ESM;
 import com.aware.utils.Aware_Plugin;
 import com.aware.utils.Scheduler;
-
 import org.json.JSONException;
 
 import java.util.Calendar;
@@ -86,83 +83,86 @@ public class Plugin extends Aware_Plugin {
                 null, null);
 
         if (aware_calendars != null && aware_calendars.moveToFirst()) {
-
-            if (Aware.DEBUG) {
-                Log.d(Aware.TAG, "Checking calendar " + aware_calendars.getString(1) + " for new ESMs...");
-            }
-
-            Cursor allesms = getContentResolver().query(CalendarContract.Events.CONTENT_URI,
-                    null,
-                    CalendarContract.Events.CALENDAR_ID + "=" + aware_calendars.getInt(aware_calendars.getColumnIndex(CalendarContract.Calendars._ID)) + " AND " +
-                            CalendarContract.Events.TITLE + " LIKE 'ESM%'",
-                    null,
-                    CalendarContract.Events.DTSTART + " ASC");
-
-            if (allesms != null && allesms.moveToFirst()) {
+            do {
 
                 if (Aware.DEBUG) {
-                    Log.d(Aware.TAG, "Found " + allesms.getCount() + " ESMs in calendar");
+                    Log.d(Aware.TAG, "Checking calendar " + aware_calendars.getString(1) + " for new ESMs...");
                 }
 
-                do {
-                    Calendar now = Calendar.getInstance();
-                    Calendar endToday = Calendar.getInstance();
-                    endToday.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH), 23, 59, 59);
+                Cursor allesms = getContentResolver().query(CalendarContract.Events.CONTENT_URI,
+                        null,
+                        CalendarContract.Events.CALENDAR_ID + "=" + aware_calendars.getInt(aware_calendars.getColumnIndex(CalendarContract.Calendars._ID)) + " AND " +
+                                CalendarContract.Events.TITLE + " LIKE 'ESM%'",
+                        null,
+                        CalendarContract.Events.DTSTART + " ASC");
 
-                    Uri.Builder uriBuilder = CalendarContract.Instances.CONTENT_URI.buildUpon();
-                    ContentUris.appendId(uriBuilder, now.getTimeInMillis());
-                    ContentUris.appendId(uriBuilder, endToday.getTimeInMillis());
+                if (allesms != null && allesms.moveToFirst()) {
 
-                    String[] fields = new String[]{
-                            CalendarContract.Instances._ID,
-                            CalendarContract.Instances.TITLE,
-                            CalendarContract.Instances.DESCRIPTION,
-                            CalendarContract.Instances.BEGIN
-                    };
-
-                    Cursor instances = getContentResolver().query(uriBuilder.build(), fields, CalendarContract.Instances.EVENT_ID + "=" + allesms.getInt(allesms.getColumnIndex(CalendarContract.Events._ID)), null, null);
-                    if (instances != null && instances.moveToFirst()) {
-                        do {
-                            try {
-                                Scheduler.Schedule esm = Scheduler.getSchedule(getApplicationContext(), instances.getString(instances.getColumnIndex(CalendarContract.Instances.TITLE)));
-                                if (esm == null) { //not set yet
-                                    Calendar timer = Calendar.getInstance();
-                                    timer.setTimeInMillis(instances.getLong(instances.getColumnIndex(CalendarContract.Instances.BEGIN)));
-
-                                    esm = new Scheduler.Schedule(instances.getString(instances.getColumnIndex(CalendarContract.Instances.TITLE)));
-                                    esm.setActionType(Scheduler.ACTION_TYPE_BROADCAST);
-                                    esm.setActionIntentAction(ESM.ACTION_AWARE_QUEUE_ESM);
-                                    esm.addActionExtra(ESM.EXTRA_ESM, cleanString(instances.getString(instances.getColumnIndex(CalendarContract.Instances.DESCRIPTION))));
-                                    esm.setTimer(timer);
-
-                                    Scheduler.saveSchedule(getApplication(), esm);
-
-                                } else { //check if time or the ESM definition has changed since the last time we scheduled it
-                                    if (esm.getTimer() != instances.getLong(instances.getColumnIndex(CalendarContract.Instances.BEGIN))) {
-                                        Calendar newTimer = Calendar.getInstance();
-                                        newTimer.setTimeInMillis(instances.getLong(instances.getColumnIndex(CalendarContract.Instances.BEGIN)));
-                                        esm.setTimer(newTimer);
-                                    }
-                                    if (!esm.getActionExtras().getString(0).equalsIgnoreCase(cleanString(instances.getString(instances.getColumnIndex(CalendarContract.Instances.DESCRIPTION))))) {
-                                        esm.addActionExtra(ESM.EXTRA_ESM, cleanString(instances.getString(instances.getColumnIndex(CalendarContract.Instances.DESCRIPTION))));
-                                    }
-                                    Scheduler.saveSchedule(getApplicationContext(), esm);
-                                }
-
-                                if (Aware.DEBUG) {
-                                    Log.d(Aware.TAG, "Calendar ESM saved: " + esm.toString());
-                                }
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        } while (instances.moveToNext());
+                    if (Aware.DEBUG) {
+                        Log.d(Aware.TAG, "Found " + allesms.getCount() + " ESMs in calendar");
                     }
-                    if (instances != null && !instances.isClosed()) instances.close();
 
-                } while (allesms.moveToNext());
-            }
-            if (allesms != null && !allesms.isClosed()) allesms.close();
+                    do {
+                        Calendar now = Calendar.getInstance();
+                        Calendar endToday = Calendar.getInstance();
+                        endToday.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH), 23, 59, 59);
+
+                        Uri.Builder uriBuilder = CalendarContract.Instances.CONTENT_URI.buildUpon();
+                        ContentUris.appendId(uriBuilder, now.getTimeInMillis());
+                        ContentUris.appendId(uriBuilder, endToday.getTimeInMillis());
+
+                        String[] fields = new String[]{
+                                CalendarContract.Instances._ID,
+                                CalendarContract.Instances.TITLE,
+                                CalendarContract.Instances.DESCRIPTION,
+                                CalendarContract.Instances.BEGIN
+                        };
+
+                        Cursor instances = getContentResolver().query(uriBuilder.build(), fields, CalendarContract.Instances.EVENT_ID + "=" + allesms.getInt(allesms.getColumnIndex(CalendarContract.Events._ID)), null, null);
+                        if (instances != null && instances.moveToFirst()) {
+                            do {
+                                try {
+                                    Scheduler.Schedule esm = Scheduler.getSchedule(getApplicationContext(), instances.getString(instances.getColumnIndex(CalendarContract.Instances.TITLE)));
+                                    if (esm == null) { //not set yet
+                                        Calendar timer = Calendar.getInstance();
+                                        timer.setTimeInMillis(instances.getLong(instances.getColumnIndex(CalendarContract.Instances.BEGIN)));
+
+                                        esm = new Scheduler.Schedule(instances.getString(instances.getColumnIndex(CalendarContract.Instances.TITLE)));
+                                        esm.setActionType(Scheduler.ACTION_TYPE_BROADCAST);
+                                        esm.setActionIntentAction(ESM.ACTION_AWARE_QUEUE_ESM);
+                                        esm.addActionExtra(ESM.EXTRA_ESM, cleanString(instances.getString(instances.getColumnIndex(CalendarContract.Instances.DESCRIPTION))));
+                                        esm.setTimer(timer);
+
+                                        Scheduler.saveSchedule(getApplication(), esm);
+
+                                    } else { //check if time or the ESM definition has changed since the last time we scheduled it
+                                        if (esm.getTimer() != instances.getLong(instances.getColumnIndex(CalendarContract.Instances.BEGIN))) {
+                                            Calendar newTimer = Calendar.getInstance();
+                                            newTimer.setTimeInMillis(instances.getLong(instances.getColumnIndex(CalendarContract.Instances.BEGIN)));
+                                            esm.setTimer(newTimer);
+                                        }
+                                        if (!esm.getActionExtras().getString(0).equalsIgnoreCase(cleanString(instances.getString(instances.getColumnIndex(CalendarContract.Instances.DESCRIPTION))))) {
+                                            esm.addActionExtra(ESM.EXTRA_ESM, cleanString(instances.getString(instances.getColumnIndex(CalendarContract.Instances.DESCRIPTION))));
+                                        }
+                                        Scheduler.saveSchedule(getApplicationContext(), esm);
+                                    }
+
+                                    if (Aware.DEBUG) {
+                                        Log.d(Aware.TAG, "Calendar ESM saved: " + esm.toString());
+                                    }
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            } while (instances.moveToNext());
+                        }
+                        if (instances != null && !instances.isClosed()) instances.close();
+
+                    } while (allesms.moveToNext());
+                }
+                if (allesms != null && !allesms.isClosed()) allesms.close();
+
+            } while (aware_calendars.moveToNext());
         }
         if (aware_calendars != null && !aware_calendars.isClosed()) aware_calendars.close();
     }
